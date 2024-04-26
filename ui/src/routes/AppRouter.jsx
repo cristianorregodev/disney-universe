49725dfe3417895f108e4c4a06307533b47e4213
cristianorregodev <cristian.duquew@gmail.com>
2024-04-26 08:10:43 -0500

    Feat (routes): Agrega rutas para mostrar pagina personajes y peliculas

--- ui/src/routes/AppRouter.jsx
+++ ui/src/routes/AppRouter.jsx
@@ -3,15 +3,17 @@ import { Layout } from '../containers/Layout'
 import { Home } from '../pages/Home'
 import { CharacterDetails } from '../pages/CharacterDetails'
 import { MovieDetails } from '../pages/MovieDetails'
+import { Movies } from '../pages/Movies'
+import { Characters } from '../pages/Characters'
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/characters" element={<h1>Hola desde persoanjes</h1>} />
+          <Route path="/characters" element={<Characters />} />
           <Route path="/characters/:id" element={<CharacterDetails />} />
-          <Route path="/movies" element={<h1>Hola desde peliculas</h1>} />
+          <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:id" element={<MovieDetails />} />
         </Routes>
       </Layout>
